4e907b0a3a2ad29541ce5e5cdc4c61d8afc5a8f6
osboxes <yash.govil@gmail.com>
2020-02-16 15:36:47 -0500

    working dhcp server and config, need to figure out how to hop IP

--- mininet/DHCP_Topo.py
+++ mininet/DHCP_Topo.py
@@ -85,46 +85,24 @@ type: sudo python mininet/examples/Ryuretic/DHCP_Topo.py
 """
 
 from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.link import TCLink
-from mininet.util import custom, quietRun
-from mininet.node import RemoteController,OVSSwitch
-from mininet.log import setLogLevel, info, lg
-from mininet.nodelib import NAT
-from mininet.cli import CLI
-from mininet.util import run
-
 
 #Topology to be instantiated in Mininet
 
-DNSTemplate = """
-    start	10.0.0.50
-    end		10.0.0.60
-    option	subnet	255.255.255.0
-    option	domain	local
-    option	lease	60  # seconds
-    """
 class dhcpTopo(Topo):
     "Mininet DHCP Test topology"
     
     def __init__(self, cpu=.1, max_queue_size=None, **params):
         '''
-        nat+--+              +-----------+h1
-              |              +-----------+h2 (evil)
-              +-------+s1+---+-----------+h3
-                DHC+---+     +-----------+h4
-                             +-----------+h5
-                             +-----------+h6
+          +---------------+host
+        s1+----DHCP
+          +---------------+serv
         '''
         # Initialize topo
         Topo.__init__(self, **params)
         ###Thanks to Sean Donivan for the NAT code####
-        natIP= '10.0.0.254'
-        # Host and link configuration
-        hostConfig = {'cpu': cpu, 'defaultRoute': 'via ' + natIP }
-        LinkConfig = {'bw': 10, 'delay': '1ms', 'loss': 0,
+        LinkConfig = {'delay': '1ms',
                    'max_queue_size': max_queue_size }
-        LinkConfig2 = {'bw': 10, 'delay': '500ms', 'loss': 0,
+        LinkConfig2 = {'delay': '5ms',
                    'max_queue_size': max_queue_size }
         #################################################
 
@@ -132,141 +110,14 @@ class dhcpTopo(Topo):
         s1  = self.addSwitch('s1')
 
         # add DHCP server with slightly longer delay
-        dhcpS = self.addHost('dhcpS', ip='10.0.0.200/24')
-        self.addLink(s1, dhcpS, 1, 1, **LinkConfig2)
+        dhcp = self.addHost('dhcp', ip='10.0.1.200/24')
+        self.addLink(s1, dhcp)
         
-        #add six hosts with no assigned IP
-        h1 = self.addHost('h1', ip=None, **hostConfig)
-        self.addLink(s1, h1, 2, 1, **LinkConfig)
+        #add one hosts with no assigned IP and 1 with assigned
+        host = self.addHost('host', ip=None)
+        self.addLink(s1, host)
         
-        h2 = self.addHost('h2', ip=None)
-        self.addLink(s1, h2, 3, 1 )
-        
-        h3 = self.addHost('h3', ip=None)
-        self.addLink(s1, h3, 4, 1, **LinkConfig)
-        
-        h4 = self.addHost('h4', ip=None, **hostConfig)
-        self.addLink(s1, h4, 5, 1, **LinkConfig)
-        
-        h5 = self.addHost('h5', ip=None, **hostConfig)
-        self.addLink(s1, h5, 6, 1, **LinkConfig)
-        
-        h6 = self.addHost('h6', ip=None, **hostConfig)
-        self.addLink(s1, h6, 7, 1, **LinkConfig)
+        serv = self.addHost('serv')
+        self.addLink(s1, serv)
 
-        # Create and add NAT
-        self.nat = self.addNode( 'nat', cls=NAT, ip=natIP,
-                            inNamespace=False)	
-        self.addLink(s1, self.nat, port1=8 )
-
-def startWireShark(host):
-    print('***Starting Wireshark...*** \n')
-    host.cmd('sudo wireshark &')
-    raw_input("\nPress Enter once Wireshark is capturing traffic \n")    
-
-def makeRogueConfig( filename, intf, dns ):
-    "Create a DHCP configuration file"
-    
-    config = (
-        'interface %s' % intf, 
-		DNSTemplate,
-        'option router %s' % natIP,
-        'option dns %s' % dns,
-        '' )
-    with open( filename, 'w' ) as f:
-        f.write( '\n'.join( config ) )
-
-def startRogueDHCP( host, dns ):
-    "Start DHCP server on host with specified DNS server"
-    print '* Starting Rogue DHCP server on ', host, 'at', dns, '\n'
-    print 'host default interface ', host.defaultIntf()
-    dhcpConfig = '/tmp/%s-udhcpd.conf' % host
-    makeRogueConfig( dhcpConfig, host.defaultIntf(), dns )
-    print host.cmd( 'udhcpd -f', dhcpConfig,
-              '1>/tmp/%s-dhcp.log 2>&1  &' % host )
-    print "**UDHCPD from ", dhcpConfig, " is running.**"
-
-def stopRogueDHCP( host, dns ):
-    "Stop DHCP server on host"
-    print( '* Stopping DHCP server on', host, 'at', dns, '\n' )
-    host.cmd( 'kill %udhcpd' )
-    
-# DHCP client functions
-def startDHCPclient( host ):
-    print "Start DHCP client on", host
-    intf = host.defaultIntf()
-    print 'Intf for ', host, ' is ', intf
-    print host.cmd( 'dhclient -v -d -r', intf )
-    print host.cmd( 'dhclient -v -d -1> /tmp/dhclient.log 2>&1', intf, '&' )
-
-def stopDHCPclient( host ):
-    host.cmd( 'kill %dhclient' )
-
-
-if __name__ == '__main__':
-    print('*** Starting Mininet *** \n')
-    topo = dhcpTopo()
-    net = Mininet(topo=topo, link=TCLink, controller=RemoteController)
-    print('*** Topology Created *** \n')
-    net.start()
-    run("ovs-vsctl set bridge s1 protocols=OpenFlow13")
-    dhcpS, h1, h2 = net.hosts[0], net.hosts[1], net.hosts[2]
-    s1,h3,h4 = net.switches[0], net.hosts[3], net.hosts[4]
-    #startWireShark(h1)
-    print('***Attempting to start dhcp server***')
-    print dhcpS.cmd('sudo /etc/init.d/isc-dhcp-server start')
-#    raw_input("\nPress Enter for dhcp server \n")
-    #startWireShark(dhcpS)
-#    print('*** Assigning IP to h1 and h2 (See Wireshark) *** \n')
-#    print h1.cmd('dhclient')
-#    print h2.cmd('dhclient')
-    # Now start Rogue DHCP server
-    ############################################
-    #print '***Starting Rogue DHCP server***'
-    #serIP = h2.cmd('hostname -I')
-    #startRogueDHCP( h2, serIP)
-    #print '***Starting DHCP client***'
-#    print h3.cmd('dhclient')
-#    print h4.cmd('dhclient')
-    ############################################
-    print('*** Running CLI *** \n')
-    CLI( net )
-    print 'Stopping Network'
-    #stopRogueDHCP( h2, serIP )
-    #stopDHCPclient( h3 )
-    dhcpS.cmd('sudo /etc/init.d/isc-dhcp-server stop')
-    net.stop()
-
-# ps -A | grep controller
-# sudo fuser -k 6633/tcp
-# pyretic.py -v high pyretic.modules.arp
-# sudo fuser -k 6633/tcp
-# nmap -sS -sU -PN 10.0.1.21
-
-
-
-"""
--d Force dhclient to run as a foreground process.
--r Tell dhclient to release the current lease it has from the server. 
--v Enable verbose log messages.
--1 Try  to  get  a  lease  once. 
-"""
-
-"""
-Possible issues compare dhcpd.conf file to dhclient.conf. 
-Adjust IP range for dhclient.conf and server files. 
-"""      
-
-
-
-
-
-"""
-option rfc3442-classless-static-routes code 121 = array of unsigned integer 8;
-request subnet-mask, broadcast-address, time-offset, routers,
-        domain-name, domain-name-servers, domain-search, host-name,
-        dhcp6.name-servers, dhcp6.domain-search,
-        netbios-name-servers, netbios-scope, interface-mtu,
-        rfc3442-classless-static-routes, ntp-servers,
-        dhcp6.fqdn, dhcp6.sntp-servers;
-"""
+topos = { 'mytopo': ( lambda: dhcpTopo() ) }
